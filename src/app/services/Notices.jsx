21745a4873c0f147fc68b1958bc271955160019b
Marcos Guilherme <malves@hsolmkt.com.br>
2021-01-27 18:29:19 -0300

    create services, sass, bootstrap e rafatoring

--- src/app/services/Notices.jsx
+++ src/app/services/Notices.jsx
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import Services from '../providers/Services';
 
+// const configs = {
+//   'Content-type': 'application/json',
+//   'Authorization': 'Bearer ' + localStorage.getItem('token')
+// }
 class Notices extends Services {
   /**
    * Get Notices Api.
    * @param {object} data
    */
-  async notices(data = {}) {
-    return axios.get(this.withParams(`${process.env.REACT_APP_API_URL}/campaigns/${campaign_id}/graphics/suppliers`, data));
+  async getNotices(data = {}) {
+    return axios.get(this.withParams(`${process.env.REACT_APP_API_URL}/news`, data));
   }
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import Dashboard from '../../layouts/Dashboard';
-
+import NoticeService from '../../app/services/Notices';
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            news: [],
+            loading: true,
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({
+          loading: true
+        }, async () => {
+          this.getNoticesAll();
+        });
+    }
+
+    getNoticesAll = async () => {    
+        this.setState({
+          loading: true,
+        }, () => {
+            NoticeService.getNotices({
+          }).then(({ data }) => {
+            this.setState({
+                news: data,
+                loading: false,
+            });
+          });
+        });
+    }
     render() {
+        const { news } = this.state;
         return (
             <Dashboard>
-                <h1>Teste</h1>
-            </Dashboard>
+                <div className="row">
+                    <div className="col">
+                        <div className="container">  
+                        {news.map((news, index) => {
+                            return <div className="card" id="home" key={index}>
+                            <div className="card-body">
+                                <h2 className="card-title">{news.title}</h2>
+                                <p className="card-text">{news.descript}</p>
+                                <h6 className="card-title">Categoria: {news.categories.title}</h6>
+                                <a href="#" className="btn btn-primary">Leia</a>
+                            </div>
+                        </div>
+                        })}
+                        </div>
+                    </div>
+                </div>
+            </Dashboard> 
         )
     }
 }
